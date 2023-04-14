3c60b1c831717ae3b94194adc419d488db64d01e
ashwini <ashwinign.7871@gmail.com>
2023-04-14 16:51:07 +0530

    git testing

--- EBCalculator.java
+++ EBCalculator.java
@@ -4,15 +4,25 @@ class EBCalculator
 public static void main(String[] args)
 {
 EBCalculator reader = new EBCalculator();
-reader.calculate_units();
-int amount =500;
+int bill=reader.calculate_units();
+System.out.println("one\t"+bill);
+int amount = 500;
+// int balance = amount - bill;
+// System.out.println(balance);
+reader.pay(amount,bill);
 }
+ void pay(int amount11, int bill22)
+{
+System.out.println("two\t"+(amount11- bill22));
+
+}  
 int calculate_units()
 {
 int units = 150;
 int price = 2;
-System.out.println(units*price); 
+System.out.println("three\t"+(units*price)); 
 return units*price;
+
 }
 
 }
